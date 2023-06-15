5bc41db1cc488b7fa494cb5fdcbaec760184ee11
yunju <yunju@10.96.121.61>
2023-06-15 14:57:04 +0900

    1. 두 개의 문자열을 입력받은 후 순서를 바꿔서 출력

---
+++ CodingTest/src/suhang/Str_1211.java
@@ -0,0 +1,21 @@
+package suhang;
+
+import java.util.Scanner;
+
+public class Str_1211 {	//1-1
+
+	public static void main(String[] args) {
+		
+		String S1, S2;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자를 입력하세요: ");
+		S1 = sc.next();
+		System.out.print("문자를 입력하세요: ");
+		S2 = sc.next();
+		
+		System.out.println(S2 + " " + S1);
+
+	}
+
+}
