3b4bfdffac09ff581c460d7975a3c3feaa93ffcd
yunju <yunju@10.96.121.61>
2023-06-15 23:28:30 +0900

    4. for 문을 이용하여 1부터 100까지의 홀수를 모두 출력하는 프로그램을 작성

---
+++ LoopPractice/src/forpractice/Odd1to100.java
@@ -0,0 +1,16 @@
+package forpractice;
+
+public class Odd1to100 {
+
+	public static void main(String[] args) {
+
+		for(int i=0; i<=100; i++){
+			if(i%2 == 1) {
+				int sum = i;
+				System.out.println(sum);
+			}
+		}
+
+	}
+
+}
