d22681d4c12a815ba7a50a2e6abf34d8c9888e6c
yunju <yunju@10.96.121.61>
2023-06-21 19:34:22 +0900

    8. 빛이 하루 동안 가는 거리(km)를 구하기. 빛은 1초에 약 300000km를 이동.

---
+++ CodingTest/src/suhang/Light_1211.java
@@ -0,0 +1,19 @@
+package suhang;
+
+public class Light_1211 {
+
+	public static void main(String[] args) {
+		long light = 300000, min, sec, sum;
+		min = 24*60;
+		sec = min*60;
+		sum = sec*light;  
+		
+		
+		System.out.println("빛이 하루 동안 가는 거리 : " + sum + "km");
+		
+		
+		
+
+	}
+
+}
