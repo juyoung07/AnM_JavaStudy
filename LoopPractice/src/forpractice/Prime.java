ea213fdea947aed3bda206cba87eb1eb669ea810
juyoung07 <yunju5090@gmail.com>
2023-07-26 00:19:01 +0900

    6-2. 입력 받은 수가 소수인지 판별하는 프로그램

--- LoopPractice/src/forpractice/Prime.java
+++ LoopPractice/src/forpractice/Prime.java
@@ -4,15 +4,27 @@ public class Prime {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요: ");
+		
+		boolean prime = false ;
 		int n = sc.nextInt();
 		
-		if((n%2==1) || (n%3==1) || (n%3==2)) {
-			System.out.println(n + "은 소수이다.");
+		for(int i=2; i<n; i++) {
+		// 1은 소수도 합성수도 아니기 때문에 2부터 시작
+			if(n%i==0) {
+				prime = true;
+				break;
+			}
+			break;
+		}
+		
+		if(prime) {
+			System.out.println(n + "은(는) 소수가 아닙니다.");
 		}
 		else {
-			System.out.println(n + "은 소수가 아니다.");
+			System.out.println(n + "은(는) 소수입니다.");
 		}
-
+			
 	}
 
 }
