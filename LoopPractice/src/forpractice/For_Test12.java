3d41b79c8db12f45a2b7db9f89a93e266818b84c
juyoung07 <yunju5090@gmail.com>
2023-08-01 22:33:08 +0900

    11-2. a+b=14를 만족하는 한자리 자연수 a, b를 모두 구하는 프로그램

---
+++ LoopPractice/src/forpractice/For_Test12.java
@@ -0,0 +1,15 @@
+package forpractice;
+
+public class For_Test12 {
+
+	public static void main(String[] args) {
+		for(int a=1; a<=9; a++) {
+			for(int b=1; b<=9; b++) {
+				if(a+b==14)
+					System.out.println("a = " + a + ", b = " + b);
+			}
+		}
+
+	}
+
+}
