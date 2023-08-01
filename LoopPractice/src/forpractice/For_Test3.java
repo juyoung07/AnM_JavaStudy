acb7334f1f16486f0dc15d12cee1236be72370a3
juyoung07 <yunju5090@gmail.com>
2023-08-01 21:48:32 +0900

    7-3. 1+(1/2)+(1/3)+...+(1/100)을 구하는 프로그램

---
+++ LoopPractice/src/forpractice/For_Test3.java
@@ -0,0 +1,15 @@
+package forpractice;
+
+public class For_Test3 {
+
+	public static void main(String[] args) {
+		int i;
+		float sum=0;
+		for(i=1; i<=100; i++) {
+			sum += (float)(1/i);
+		}
+		System.out.println(sum);
+
+	}
+
+}
