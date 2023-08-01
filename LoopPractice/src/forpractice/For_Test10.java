ec7fb2cbe4227adb22cc487e0d3ae988d4aabbcc
juyoung07 <yunju5090@gmail.com>
2023-08-01 21:37:42 +0900

    10-2. 1+2=3, 3+3=6, 6+4=10,...처럼 1부터 20까지의 수를 계속하여 더하면서 그 때마다 합의 결과를 출력하는 프로그램

---
+++ LoopPractice/src/forpractice/For_Test10.java
@@ -0,0 +1,18 @@
+package forpractice;
+
+public class For_Test10 {
+
+	public static void main(String[] args) {
+		int n=1, sum=0;
+		for(int i=2; i<=20; i++) {
+			sum = i+n;
+			System.out.println(n + "+" + i + " = " + sum);
+			n = sum;
+			
+			
+			
+		}
+
+	}
+
+}
